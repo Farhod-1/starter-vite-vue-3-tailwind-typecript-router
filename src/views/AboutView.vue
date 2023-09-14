61790d049695b05cc05c28909e174f07fbc0870e
Farxod <farhodqarshiyev@gmail.com>
2023-09-14 10:14:31 +0500

    ready

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="text-red-600">
+  hello world this is about view
+</div>
+  <router-link to="/">Home</router-link>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="text-red-600">
+    this is home view
+  </div>
+  <router-link to="/about">About</router-link>
+</template>
+
+<style scoped>
+
+</style>
